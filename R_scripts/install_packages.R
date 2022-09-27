50a9f9fbc5edcb6e1182ff4c4d0014be9ce31ca5
Joe Guinness <joeguinness@gmail.com>
2022-09-27 15:38:50 -0400

    comment/uncomment

--- R_scripts/install_packages.R
+++ R_scripts/install_packages.R
@@ -1,7 +1,7 @@
 
 #install.packages("devtools", repos="https://cloud.r-project.org")
-#install.packages("rootSolve", repos="https://cloud.r-project.org")
 #install.packages("fields", repos="https://cloud.r-project.org")
 install.packages("matrixcalc", repos="https://cloud.r-project.org")
-devtools::build("../../GpGpm")
+install.packages("rootSolve", repos="https://cloud.r-project.org")
+#devtools::build("../../GpGpm")
 install.packages("../../GpGpm_0.4.0.tar.gz", type = "source", repos = NULL )
