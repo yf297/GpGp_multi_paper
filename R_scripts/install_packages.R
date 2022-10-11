41c04fd7ed331c8c885fd9a512477e476a18900b
Joe Guinness <joeguinness@gmail.com>
2022-10-11 08:43:31 -0400

    change path to package

--- R_scripts/install_packages.R
+++ R_scripts/install_packages.R
@@ -3,5 +3,4 @@
 install.packages("fields", repos="https://cloud.r-project.org")
 install.packages("matrixcalc", repos="https://cloud.r-project.org")
 install.packages("rootSolve", repos="https://cloud.r-project.org")
-#devtools::build("../../GpGpm")
-install.packages("../../GpGpm_0.4.0.tar.gz", type = "source", repos = NULL )
+install.packages("../GpGpm_0.4.0.tar.gz", type = "source", repos = NULL )
